1af03e47042586d5db82466a3cbe4ba7cef66f2b
vadim <lzaitsev@gmail.com>
2025-12-19 22:05:01 +0500

    Connect UserOrm to RoleOrm via ORM relationships

--- microshop/core/models/user.py
+++ microshop/core/models/user.py
@@ -1,12 +1,18 @@
+from typing import TYPE_CHECKING
+
 from datetime import datetime
 
 from sqlalchemy import String, text, DateTime
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
+from .mixin import RelationMixin
+
+if TYPE_CHECKING:
+    from .role import RoleOrm
 
 
-class UserOrm(Base):
+class UserOrm(RelationMixin, Base):
     __tablename__ = 'users'
 
     user_id: Mapped[int] = mapped_column(primary_key=True)
@@ -33,3 +39,12 @@ class UserOrm(Base):
         DateTime,
         nullable=True,
     )
+
+    # Настраиваем Mixin для связи с Role
+    _target_table = "roles"
+    _target_column = "role_id"
+    _back_populates = "users"
+    _unique = False
+    _nullable = False
+
+    roles: Mapped['RoleOrm'] = relationship(back_populates=_back_populates)
