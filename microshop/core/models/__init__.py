beca5a0295061b6cb77c6ae78a5a7db7f4e214bf
vadim <lzaitsev@gmail.com>
2026-01-07 16:06:48 +0500

    create permissions table

--- microshop/core/models/__init__.py
+++ microshop/core/models/__init__.py
@@ -3,5 +3,6 @@ from .product import Product
 from .role import RoleOrm
 from .user import UserOrm
 from .permissions import PermissionOrm
+# from .role_permissions import RolePermissionsAssoc
 
 __all__ = ['Base', Product, RoleOrm, UserOrm, PermissionOrm]
--- microshop/core/models/permissions.py
+++ microshop/core/models/permissions.py
@@ -1,6 +1,12 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from typing import TYPE_CHECKING
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
+
+
+if TYPE_CHECKING:
+    from .role import RoleOrm
 
 
 class PermissionOrm(Base):
@@ -9,3 +15,8 @@ class PermissionOrm(Base):
     permission_id: Mapped[int] = mapped_column(primary_key=True)
     permission_code: Mapped[str]
     permission_description: Mapped[str]
+
+    # roles: Mapped[list['RoleOrm']] = relationship(
+    #     secondary='role_permissions',
+    #     back_populates='permissions',
+    # )
